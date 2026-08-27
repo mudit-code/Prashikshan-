7aa16e81071ae68d885c2eceb171962ed6f07716
mudit-code <233256@kit.ac.in>
2026-08-27 21:40:57 +0530

    fix: add ssl to db and fix connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,6 +6,7 @@ const poolConfig = process.env.DATABASE_URL
       connectionString: process.env.DATABASE_URL,
       max: 10,
       idleTimeoutMillis: 30000,
+      ssl: { rejectUnauthorized: false }
     }
   : {
       host: process.env.DB_HOST,
